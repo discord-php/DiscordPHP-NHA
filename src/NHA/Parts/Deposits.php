80a017cf2be851d6db8932551b463c4b7c9c0a1d
Valithor Obsidion <valithor@discordphp.org>
2026-08-30 22:05:47 -0400

    Add missing parts

---
+++ src/NHA/Parts/Deposits.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP-NHA project.
+ *
+ * Copyright (c) 2025-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace NHA\Parts;
+
+/**
+ * A lightweight, read-only wrapper around deposit information.
+ *
+ * @since 0.1.0
+ */
+class Deposits extends Out
+{
+    /** @inheritdoc */
+    protected $attributes = [
+        'deposits',
+    ];
+}
---
+++ src/NHA/Parts/Scene.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP-NHA project.
+ *
+ * Copyright (c) 2025-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace NHA\Parts;
+
+/**
+ * A lightweight, read-only wrapper around a scene description.
+ *
+ * @since 0.1.0
+ */
+class Scene extends Out
+{
+    /** @inheritdoc */
+    protected $attributes = [
+        'description',
+        'entities',
+        'items',
+    ];
+}
