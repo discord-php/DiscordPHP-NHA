c12d95e6985aea054dd31e9782818711602c27e3
Valithor Obsidion <valithor@discordphp.org>
2026-08-31 11:28:41 -0400

    Update Client.php

--- src/NHA/Client/Client.php
+++ src/NHA/Client/Client.php
@@ -14,6 +14,12 @@ declare(strict_types=1);
 namespace NHA\Client;
 
 use Discord\Parts\User\Client as DiscordClient;
+use Discord\Repository\EmojiRepository;
+use Discord\Repository\GuildRepository;
+use Discord\Repository\PrivateChannelRepository;
+use Discord\Repository\SoundRepository;
+use Discord\Repository\StickerPackRepository;
+use Discord\Repository\UserRepository;
 use NHA\Repository\AgentRepository;
 use NHA\Repository\DiscoveryRepository;
 use NHA\Repository\EconomyRepository;
@@ -24,6 +30,12 @@ use NHA\Repository\WorldRepository;
 /**
  * The NHA Client class.
  *
+ * @property EmojiRepository $emojis
+ * @property GuildRepository $guilds
+ * @property PrivateChannelRepository $private_channels
+ * @property SoundRepository $sounds
+ * @property StickerPackRepository $sticker_packs
+ * @property UserRepository $users
  * @property AgentRepository $agents
  * @property DiscoveryRepository $discovery
  * @property EconomyRepository $economy
@@ -37,6 +49,12 @@ class Client extends DiscordClient
      * @inheritDoc
      */
     protected $repositories = [
+        'emojis' => EmojiRepository::class,
+        'guilds' => GuildRepository::class,
+        'private_channels' => PrivateChannelRepository::class,
+        'sounds' => SoundRepository::class,
+        'sticker_packs' => StickerPackRepository::class,
+        'users' => UserRepository::class,
         'agents' => AgentRepository::class,
         'discovery' => DiscoveryRepository::class,
         'economy' => EconomyRepository::class,
